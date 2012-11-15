4c1e03bcc2c7191826da317ccaa2e8467fdaf12a
Trshant <trshant@gmail.com>
2012-11-16 01:49:41 +0530

    user accounts added , bookmarklet, merge.

---
+++ README.markdown
@@ -0,0 +1,20 @@
+# Trishmarks - lazy bookmarking
+-----------------------------
+
+This was created because the current bookmakring sites make bookmarking a chore. Trishmarks makes it easy to bookmark and go on browsing,
+ so there is no waste of your valuable time.
+
+
+## Changelog
+
+16th nov 2012
+-----------------------------
+
+* Added User Accounts
+* Can merge many bookmarks to form one bookmark with a huge amount of data
+
+Todo's
+--------
+
+* the saving should be smoother
+* edit form and merge form
--- main.py
+++ main.py
@@ -23,21 +23,35 @@ import json
 from google.appengine.api import users
 from google.appengine.ext import db
 
-Location_url = 'http://localhost:8081'
+Location_url = 'http://trshant-bookmarks.appspot.com' ## 'http://localhost:8081' ## 'http://trshant-bookmarks.appspot.com'
 
-# , encoding="latin-1"
+def safe_unicode(obj, *args):
+    """ return the unicode representation of obj """
+    try:
+        return unicode(obj, *args)
+    except UnicodeDecodeError:
+        # obj is byte string
+        ascii_text = str(obj).encode('string_escape')
+        return unicode(ascii_text)
+
+def unquote_u(source):
+  result = source
+  if '%u' in result:
+    result = result.replace('%u','\\u').decode('unicode_escape')
+  result = urllib.unquote(result)
+  return result
 
 
 class bookmark(db.Model):
   url = db.StringProperty()
   title = db.StringProperty()
-  content = db.StringProperty(multiline=True)
+  content = db.TextProperty()  ##db.StringProperty(multiline=True)
   user = db.StringProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = "text/html; charset=utf-8"
-        self.response.out.write("""Welcome to <a href="">[trishmarks]</a> .""")
+        self.response.out.write("""Welcome to <a href="javascript:((function(){Q='';ss='%s/save?c=';x=document;y=window;if(x.selection){Q=x.selection.createRange().text}else if(y.getSelection){Q=y.getSelection()}else if(x.getSelection){Q=x.getSelection()}for(i=0,l=Q.rangeCount;i<l;i++){ss+=escape(Q.getRangeAt(i).toString())+escape('<br/>')}ss+='&u='+escape(location.href)+'&t='+escape(document.title)+'';var scr=document.createElement('script');scr.setAttribute('src',ss);document.getElementsByTagName('head')[0].appendChild(scr);})())">[trishmarks]</a> .""" % Location_url)
         user = users.get_current_user()
         if user:
             greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
@@ -46,25 +60,25 @@ class MainPage(webapp2.RequestHandler):
             self.response.out.write("""<hr/>""")
             u = user.user_id();
             bookmarks = db.GqlQuery("SELECT * FROM bookmark WHERE user = :1 ", u )
-            self.response.out.write(bookmarks.count())
+           ## self.response.out.write(bookmarks.count())
             for bkm in bookmarks:
               self.response.out.write("""<a href="%s">%s</a> { <a href="delete?id=%s">delete</a> | <a href="read?u=%s" target="_blank">readability</a> }""" %
                                     ( urllib.unquote(bkm.url) , urllib.unquote(bkm.title) , bkm.key() , bkm.url  ) )
               cont = urllib.unquote( bkm.content )
               self.response.out.write('<blockquote>%s</blockquote>' % cont )
-              self.response.out.write('<sppan>%s</span><br/>' % bkm.user )
+              ##self.response.out.write('<sppan>%s</span><br/>' % bkm.user )
               
         else:
             greeting = ("Please <a href=\"%s\">Login</a>" % (users.create_login_url("/")))
             self.response.out.write("%s" % greeting)
             self.response.out.write("""<hr/>""")
-            self.response.out.write("""Please drag the bookmarklet link (This one -> <a href="">[Trishmark]</a>) to your bookmark toolbar.<br/>
+            self.response.out.write("""Please drag the bookmarklet link (This one -> <a href="javascript:((function(){Q='';ss='%s/save?c=';x=document;y=window;if(x.selection){Q=x.selection.createRange().text}else if(y.getSelection){Q=y.getSelection()}else if(x.getSelection){Q=x.getSelection()}for(i=0,l=Q.rangeCount;i<l;i++){ss+=escape(Q.getRangeAt(i).toString())+escape('<br/>')}ss+='&u='+escape(location.href)+'&t='+escape(document.title)+'';var scr=document.createElement('script');scr.setAttribute('src',ss);document.getElementsByTagName('head')[0].appendChild(scr);})())">[Trishmark]</a>) to your bookmark toolbar.<br/>
 After installing the bookmarklet you can use this by selecting part of the page and then clicking the bookmarklet.
 Your page and selected text will be saved and no time will be wasted.<br>
 This bookmarking site's USP is to keep out of your way as much as possible, but still be an invaluable assistant for the lang haul.""")
     
-        y = '%u2014'
-        self.response.out.write('%s' % y )
+        ##y = '%u2014'
+        ##self.response.out.write('%s' % y )
 
 class SaveA(webapp2.RequestHandler):
     def get(self):
@@ -108,7 +122,12 @@ class Save(webapp2.RequestHandler):
 u = "You have already bookmarked this site.\\nHowever if you wish, the new save can be merged with the old one.\\nWould you like to proceed?\\nHitting the cancel button will save the bookmark separately.";
 function mergeRequest(){
 yo = confirm(u);
-if(yo){window.open('%s','_self');}else{var scr=document.createElement('script');scr.setAttribute('src','%s');document.getElementsByTagName('head')[0].appendChild(scr);}
+if(yo){window.open('%s','_self');}
+else{
+var scr=document.createElement('script');
+scr.setAttribute('src','%s');
+document.getElementsByTagName('head')[0].appendChild(scr);
+}
 }
 mergeRequest();
 
@@ -116,12 +135,12 @@ mergeRequest();
           else:
             bm = bookmark()
             bm.url = url
-            title = self.request.get('t')
+            title = safe_unicode(unquote_u(self.request.get('t')))
             if( title == ""):
               bm.title = self.request.get('u')
             else:
               bm.title = title
-            bm.content = str(self.request.get('c'))
+            bm.content = safe_unicode(unquote_u(self.request.get('c'))) ##str(self.request.get('c'))
             bm.user = user.user_id()
             bm.put()
           
@@ -164,7 +183,7 @@ class MergeForm(webapp2.RequestHandler):
       bookmarks = db.GqlQuery("SELECT * FROM bookmark WHERE user = :1 AND url = :2", u , url )
       for bk in bookmarks:
         cc += bk.content
-      cc += self.request.get('c')
+      cc += safe_unicode(unquote_u(self.request.get('c')))
       self.response.out.write("""<script type='text/javascript'>function getContent(){document.getElementById('my-textarea').value = document.getElementById('my-content').innerHTML;}</script><div id="my-content" contenteditable="true">%s</div><form id="form" action="/mergesave" onsubmit="return getContent()" method="post"><textarea id="my-textarea" name="content" style="display:none"></textarea><input type="hidden" name="title" value="%s"><input type="hidden" name="url" value="%s"><input type="submit" value="Save Merged Bookmark"/></form>""" % (cc,title,url))
     else:
       self.redirect(users.create_login_url(self.request.uri))
@@ -175,7 +194,7 @@ class MergeSave(webapp2.RequestHandler):
         if user:
           u = user.user_id()
           url = str(self.request.get('url'))
-          content = str(self.request.get('content'))
+          content = safe_unicode(unquote_u(self.request.get('content')))
           cc = ''
           title = str(self.request.get('title'))
           bookmarks = db.GqlQuery("SELECT * FROM bookmark WHERE user = :1 AND url = :2", u , url )
@@ -187,7 +206,7 @@ class MergeSave(webapp2.RequestHandler):
           bm.url = url
           bm.user = u
           bm.title = title
-          bm .content = content
+          bm.content = content
           bm.put()
           self.redirect(cgi.escape(url))
         else:
